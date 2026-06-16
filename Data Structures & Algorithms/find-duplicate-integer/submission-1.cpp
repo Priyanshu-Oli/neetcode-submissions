51bdaecceb984aa78f9d9706a6a738dac91f1b04
Priyanshu <137142380+Priyanshu-Oli@users.noreply.github.com>
2026-06-16 09:15:55 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+
+                if ((nums[i] ^ nums[j]) == 0) {
+                    return nums[i];
+                }
+            }
+        }
+
+        return -1; // unreachable given the problem constraints
+    }
+};
